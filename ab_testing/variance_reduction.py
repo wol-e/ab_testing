b7cb5f739e73238be30d3847763c242ed18ee327
Wolfgang <wolfgang.spindeler@gmail.com>
2022-12-05 11:10:55 +0100

    adding stratifeid ttest

---
+++ ab_testing/variance_reduction.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+from scipy.stats import ttest_ind_from_stats, ttest_ind, norm
+from typing import Tuple, Dict
+
+def strartified_ttest(
+        base: np.array,
+        variant: np.array,
+        strata_base: np.array,
+        strata_variant: np.array,
+        weights: Dict,
+        alternative: str="two-sided",
+) -> Tuple[float, float]:
+    strata_by_weights_base = {}
+    strata_by_weights_variant = {}
+
+    # TODO: move to proper Exceptions
+    assert\
+        len(base) == len(strata_base) and len(variant) == len(strata_variant),\
+        "lengths of observations and strata are not consistent"
+
+    assert\
+        set(strata_base) == set(strata_variant),\
+        "The strata for base and variant need to have the same set of unique values"
+
+    for strata in [strata_base, strata_variant]:
+        _, strata_counts = np.unique(strata, return_counts=True)
+        assert strata_counts.min() >= 2, "There need to be at least 2 observations per stratum"
+
+
+    for stratum, weight in weights.items():
+        strata_by_weights_base[weight] = base[strata_base == stratum]
+        strata_by_weights_variant[weight] = variant[strata_variant == stratum]
+
+    y_hat_strat_base = np.array([weight * obs.mean() for weight, obs in strata_by_weights_base.items()]).sum()
+    y_hat_strat_variant = np.array([weight * obs.mean() for weight, obs in strata_by_weights_variant.items()]).sum()
+
+    n_base, n_variant = len(base), len(variant)
+    var_y_hat_strat_base = np.array(
+        [(weight / n_base) * obs.var() for weight, obs in strata_by_weights_base.items()]
+    ).sum()
+    var_y_hat_strat_variant = np.array(
+        [(weight / n_variant) * obs.var() for weight, obs in strata_by_weights_variant.items()]
+    ).sum()
+
+    delta_strat = y_hat_strat_variant - y_hat_strat_base
+    var_delta_strat = var_y_hat_strat_base + var_y_hat_strat_variant
+
+    t_statistic = delta_strat / np.sqrt(var_delta_strat)
+
+    # TODO: figure out the degrees of freedom for the t distribution. For now we'll use a normal distribution and
+    #  raise a warning when we have less than 30 samples per group
+    if min(n_base, n_variant) <= 30:
+        raise Warning("Warning: Results might be inaccurate when there are not more 30 observations in a group")
+
+    approx_one_sided_p_val = 1 - norm(loc=0, scale=1).cdf(abs(t_statistic))
+
+    if alternative == "two-sided":
+        p_value = approx_one_sided_p_val * 2
+    elif alternative == "one-sided":
+        p_value = approx_one_sided_p_val
+    else:
+        raise ValueError("'alternative' must be wither 'two-sided' or 'one-sided'.")
+
+    return p_value, t_statistic
+
+
+if __name__ == "__main__":
+    pass
---
+++ tests/test_variance_reduction.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from ab_testing import variance_reduction
+from pytest import approx
+from scipy.stats import norm, ttest_ind
+
+def test_stratified_ttest():
+    # TODO: add a static sample to ensure removing all randomness
+    base = np.concatenate([
+        norm(loc=0, scale=1).rvs(size=100),
+        norm(loc=10, scale=1).rvs(size=100),
+    ])
+    strata_base = np.concatenate([np.array([0] * 100), np.array([1] * 100)])
+    variant = np.concatenate([
+        norm(loc=0.1, scale=1).rvs(size=100),
+        norm(loc=10.1, scale=1).rvs(size=100),
+    ])
+    strata_variant = strata_base.copy()
+
+    # First we check consistency with usual t-test when providing only a single stratum
+    p_unstratified, t_unstratified = variance_reduction.strartified_ttest(
+        base=base,
+        variant=variant,
+        strata_base=base * 0,
+        strata_variant=variant * 0,
+        weights={0: 1},
+    )
+
+    t_check, p_check = ttest_ind(variant, base)
+    # TODO: increase tolerance when figured out the correct degrees of freedom calculation in 'startified_ttest'
+    assert (p_check, t_check) == approx((p_unstratified, t_unstratified), abs=0.001)
+
+    # Secondly we check that using the correct stratums decreases the p value
+    p_stratified, t_stratified = variance_reduction.strartified_ttest(
+        base=base,
+        variant=variant,
+        strata_base=strata_base,
+        strata_variant=strata_variant,
+        weights={0: .5, 1: .5},
+    )
+
+    print(f"\n\nInfo: p check: {p_check}, p stratified: {p_stratified}\n\n")
+
+    assert p_stratified < p_check
+
